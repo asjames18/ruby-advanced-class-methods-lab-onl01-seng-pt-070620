35eae14a203b0780f927e8d28808335b2ff7d767
Antonio James <asjames18@gmail.com>
2020-07-27 00:21:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,10 @@ def self.create
 song
 end
 def self.create_from_filename(name)
-
+  x = name.gsub(".mp3", "").split(/\-/)
+  song = self.create_by_name(x[1].strip)
+  song.artist_name = x[0].strip
+  song
 end
 
 end
